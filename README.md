99cef802d9f676c0bb3a9a85c31d155d83baff2b
Sasuke <167464302+Johneygit@users.noreply.github.com>
2024-05-28 21:59:41 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Extractor Bot
+# working repo
+# made by CrazyMindd
+
+<a href="https://heroku.com/deploy?template=https://github.com/Johneygit/ID-Pass-to-Txt-Extractor-mew">
+     <img height="30px" src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku">
+  </a>
---
+++ config.py
@@ -0,0 +1,7 @@
+api_id = 26606308
+api_hash = "84d6c59a7ae2e1bc88dfe83c92121684"
+bot_token = "7296463040:AAFzObNezZMo30F7u5btysAu4qkM-m5u998"
+auth_users = "6448912854"
+sudo_users = [int(num) for num in auth_users.split(",")]
+osowner_users = "6448912854"
+owner_users = [int(num) for num in osowner_users.split(",")]
