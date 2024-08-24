c605169febd67164c2bcf73a362ce603d1b36102
Radhagitrepo <Radha99680@gmail.com>
2024-08-24 22:53:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 26
-api_hash = "59a74"
-bot_token = "7462A5tmGJs"
-auth_users = "6"
+api_id = 21250782
+api_hash = "458eddfa15ca2c67a2c299d925b6a71e"
+bot_token = "7481527873:AAFysk-3Aw4I8dUKOdOsZe1SlLFnjEFO_Mk"
+auth_users = "7011045471"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6"
+osowner_users = "7011045471"
 owner_users = [int(num) for num in osowner_users.split(",")]
