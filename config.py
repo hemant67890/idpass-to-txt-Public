5948f5e487f302571a420eccac2197fe3e36550f
Radhagitrepo <Radha99680@gmail.com>
2024-08-25 07:41:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 21250782
-api_hash = "458eddfa15ca2c67a2c299d925b6a71e"
-bot_token = "7481527873:AAFysk-3Aw4I8dUKOdOsZe1SlLFnjEFO_Mk"
-auth_users = "7011045471"
+api_hash = "458a2c299d925b6a71e"
+bot_token = "7481527873:AAFe1SlLFnjEFO_Mk"
+auth_users = "70110471"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "7011045471"
+osowner_users = "7015471"
 owner_users = [int(num) for num in osowner_users.split(",")]
