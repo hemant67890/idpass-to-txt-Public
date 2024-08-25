b0971dba4233b619b07f55fe1a044838c0f7a779
Radhagitrepo <Radha99680@gmail.com>
2024-08-25 07:41:55 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 import os
 
 api_id = 21250782
-api_hash = os.environ.get("API_HASH", "458eddfa15ca2c67a2c299d925b6a71e")
+api_hash = os.environ.get("API_HASH", "458edca2c67a2c299d925b6a71e")
 bot_token = os.environ.get("BOT_TOKEN")
-auth_users = os.environ.get("AUTH_USERS", "7011045471")
+auth_users = os.environ.get("AUTH_USERS", "7045471")
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = os.environ.get("OWNER_USERS", "7011045471")
+osowner_users = os.environ.get("OWNER_USERS", "70145471")
 owner_users = [int(num) for num in osowner_users.split(",")]
