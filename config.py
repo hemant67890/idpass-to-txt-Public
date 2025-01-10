3d08207e433ac4c4f042dde61289b632629beb3b
hemant67890 <godarasubhash337@gmail.com>
2025-01-10 20:38:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id = 26512964
 api_hash = "e5d187c6c7a0919ccb8866f76f655701"
-bot_token = "7836714507:AAE0uMDhthtcFEPoO96xxUxXmeyW6Obz5tU"
+bot_token = "7895147183:AAGQ5XJJIvY_8viiZtbz-fyzOGLPEJdADxE"
 auth_users = "2052075731"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "2052075731"
